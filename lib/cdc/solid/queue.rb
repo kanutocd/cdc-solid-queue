9f09120c8ebef60ca0f14a12656a7e4b7ecb84f3
Ken C. Demanawa <kenneth.c.demanawa@gmail.com>
2026-06-26 12:33:52 +0800

    add boilerplate files generated from bundle gem

---
+++ lib/cdc/solid/queue.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "queue/version"
+
+module Cdc
+  module Solid
+    module Queue
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
